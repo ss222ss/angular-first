d938407ef919485230d020c05328007b4b41317e
ss222ss <emretoktas@gmail.com>
2022-01-12 12:47:23 +0300

    binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular';
+  yazi = 'merhaba hoşgeldiniz...';
+  isim: string = 'Uzay';
+  isChecked: boolean = true;
+  isCheckedFalse: boolean = false;
+  isCheckedTwoWay: boolean = true;
 }
